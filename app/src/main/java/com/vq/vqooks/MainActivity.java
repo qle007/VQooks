b0517478a920280ca705bdb5c77f65e04035488d
qle007 <qle007@fiu.edu>
2017-11-29 23:48:54 -0500

    added new activities recipes and ingredients, and added labels

--- app/src/main/java/com/vq/vqooks/MainActivity.java
+++ app/src/main/java/com/vq/vqooks/MainActivity.java
@@ -1,7 +1,10 @@
 package com.vq.vqooks;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //ingredient button
+        Button ingButton = (Button) findViewById(R.id.ingredients);
+        ingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent ingIntent = new Intent(MainActivity.this, IngredientsActivity.class);
+                startActivity(ingIntent);
+            }
+        });
+
+        //recipe button
+        Button recButton = (Button) findViewById(R.id.recipes);
+        recButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent recIntent = new Intent(MainActivity.this, RecipeActivity.class);
+                startActivity(recIntent);
+            }
+        });
+
+
     }
 }
