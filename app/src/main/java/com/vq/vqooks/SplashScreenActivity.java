fcbe85bf7f694adad96b202dd6eb530d562d90ac
qle007 <qle007@fiu.edu>
2017-11-29 22:20:34 -0500

    added splash screen

---
+++ app/src/main/java/com/vq/vqooks/SplashScreenActivity.java
@@ -0,0 +1,25 @@
+package com.vq.vqooks;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 2000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                Intent homeIntent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
