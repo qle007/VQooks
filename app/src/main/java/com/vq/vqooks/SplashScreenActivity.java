b02dddef676f294bc39e6e0f3957eab5e934f96b
vcani003 <vcani003@fiu.edu>
2017-11-29 22:32:56 -0500

    changed layout file names

--- app/src/main/java/com/vq/vqooks/SplashScreenActivity.java
+++ app/src/main/java/com/vq/vqooks/SplashScreenActivity.java
@@ -10,7 +10,7 @@ public class SplashScreenActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splashscreen);
         new Handler().postDelayed(new Runnable()
         {
             @Override
